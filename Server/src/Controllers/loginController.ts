812f8f2f36381ab86cf753e0c8ef5381dc0829f0
kuma <kumaleta2021@gmail.com>
2024-05-19 16:21:37 +0300

    login and signup implemented and styled

--- Server/src/Controllers/loginController.ts
+++ Server/src/Controllers/loginController.ts
@@ -1,2 +1,51 @@
 import { Request, Response } from "express";
-export const loginUsers = (req: Request, res: Response) => {};
+import { SignupModel } from "../models/models";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const generateToken = (id: string) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET || "", { expiresIn: "30d" });
+};
+export const loginUsers = async (req: Request, res: Response) => {
+  try {
+    console.log(req.body);
+    const { name, email, password } = req.body;
+    const userAlreadyExists = await SignupModel.findOne({ email: email });
+    if (userAlreadyExists) {
+      return res.status(400).json({ message: "user already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const userAccount = new SignupModel({
+      email: email,
+      password: hashedPassword,
+      name: name,
+    });
+    await userAccount.save();
+    if (userAccount) {
+      res.status(201).json({
+        id: userAccount.id,
+        name: userAccount.name,
+        email: userAccount.email,
+        token: generateToken(userAccount.id),
+      });
+    }
+  } catch (error: any) {
+    console.log(error);
+    let statusCode = 500;
+    let message = "Internal server error";
+
+    if (error.code === 11000) {
+      // Handling MongoDB duplicate key error
+      message = "Email already exists";
+      statusCode = 400;
+    } else if (error.name === "ValidationError") {
+      statusCode = 400;
+      message = "Validation error";
+    }
+
+    res.status(statusCode).json({
+      message,
+      status: statusCode,
+    });
+  }
+};
--- Server/src/Controllers/signupController.ts
+++ Server/src/Controllers/signupController.ts
@@ -1,2 +1,30 @@
 import { Request, Response } from "express";
-export const createUsers = async (req: Request, res: Response) => {};
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { SignupModel } from "../models/models";
+const generateToken = (id: string) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET || "", { expiresIn: "30d" });
+};
+export const createUsers = async (req: Request, res: Response) => {
+  try {
+    console.log(req.body);
+    const { email, password } = req.body;
+    const userAccount = await SignupModel.findOne({ email });
+    if (userAccount && (await bcrypt.compare(password, userAccount.password))) {
+      res.status(200).json({
+        id: userAccount._id,
+        email: userAccount.email,
+        name: userAccount.name,
+        token: generateToken(userAccount.id),
+      });
+    } else {
+      res.status(400).json({ message: "invalid user credential" });
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
